9edeb5cad7882766a94117d0b979dcb8bd66673b
pure-nomad <gl1193697@email.ccbcmd.edu>
2024-10-23 13:16:58 +0000

    implement concurrency (no ai)

--- scanner.go
+++ scanner.go
@@ -1,85 +1,110 @@
-// package main
+package main
 
-// import (
-// 	"bufio"
-// 	"fmt"
-// 	"net/http"
-// 	"os"
-// 	"time"
+import (
+	"bufio"
+	"crypto/tls"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"regexp"
+	"sync"
+	"time"
 
-// 	"github.com/fatih/color"
-// )
+	"github.com/fatih/color"
+)
 
-// var (
-// 	red    = color.New(color.FgHiRed).SprintFunc()
-// 	green  = color.New(color.FgHiGreen).SprintFunc()
-// 	purple = []func(...interface{}) string{
-// 		color.New(color.FgHiMagenta).SprintFunc(),
-// 		color.New(color.FgMagenta).SprintFunc(),
-// 		color.New(color.FgHiCyan).SprintFunc(),
-// 		color.New(color.FgCyan).SprintFunc(),
-// 	}
-// )
+var (
+	refPattern = regexp.MustCompile(`(?m)^ref:\s+refs/heads/.*`)
+	red        = color.New(color.FgHiRed).SprintFunc()
+	green      = color.New(color.FgHiGreen).SprintFunc()
+	purple     = []func(...interface{}) string{
+		color.New(color.FgHiMagenta).SprintFunc(),
+		color.New(color.FgMagenta).SprintFunc(),
+		color.New(color.FgHiCyan).SprintFunc(),
+		color.New(color.FgCyan).SprintFunc(),
+	}
+	wg                    = sync.WaitGroup{}
+	m                     = sync.Mutex{}
+	maxConcurrentRequests = 99
+	sem                   = make(chan struct{}, maxConcurrentRequests)
+)
 
-// func printTitle(text string) {
-// 	for i, char := range text {
-// 		colorIndex := i % len(purple)
-// 		fmt.Print(purple[colorIndex](string(char)))
-// 	}
-// 	fmt.Println()
-// }
+func printTitle(text string) {
+	for i, char := range text {
+		colorIndex := i % len(purple)
+		fmt.Print(purple[colorIndex](string(char)))
+	}
+	fmt.Println()
+}
 
-// func logFoundURL(url string) {
-// 	fmt.Println(green("Found URL:") + " " + url)
-// }
+func logFoundURL(url string) {
+	fmt.Println(green("Found URL:") + " " + url)
+}
 
-// func scanner(url string, w *os.File) {
-// 	resp, err := http.Get(url + "/.git/HEAD")
-// 	if err != nil {
-// 		fmt.Println("Error retrieving url: " + url)
-// 		return
-// 	}
-// 	defer resp.Body.Close()
+func scanner(url string, w *os.File) {
+	defer wg.Done()
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	resp, err := client.Get(url + "/.git/HEAD")
+	if err != nil {
+		fmt.Println("Error retrieving url: " + url)
+		return
+	}
+	defer resp.Body.Close()
 
-// 	if resp.StatusCode != 404 {
-// 		logFoundURL(url)
-// 		_, err := w.WriteString(url + "\n")
-// 		if err != nil {
-// 			fmt.Println("Error inserting url: " + url + " into valid_git_urls.txt")
-// 		}
-// 	}
-// }
+	if resp.StatusCode != 404 {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Error reading response body for url: " + url)
+			return
+		}
+		if refPattern.Match(body) {
+			logFoundURL(url)
+			_, err := w.WriteString(url + "\n")
+			if err != nil {
+				fmt.Println("Error inserting url: " + url + " into valid_git_urls.txt")
+			}
+		}
+	}
 
-// func main() {
-// 	if len(os.Args) != 2 {
-// 		fmt.Println(red("Please provide a url list!"))
-// 		os.Exit(0)
-// 	}
+}
 
-// 	helpText := "git scanner made by erg0sum"
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println(red("Please provide a url list!"))
+		os.Exit(0)
+	}
 
-// 	file, err := os.Open(os.Args[1])
-// 	if err != nil {
-// 		fmt.Println(red("Failed to load file: " + os.Args[1] + ". Is it in the same directory?"))
-// 		os.Exit(1)
-// 	}
-// 	defer file.Close()
+	helpText := "git scanner made by erg0sum"
 
-// 	outputFile, err := os.OpenFile("valid_git_urls.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-// 	if err != nil {
-// 		fmt.Println("Error opening file valid_git_urls.txt")
-// 		os.Exit(1)
-// 	}
-// 	defer outputFile.Close()
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println(red("Failed to load file: " + os.Args[1] + ". Is it in the same directory?"))
+		os.Exit(1)
+	}
+	defer file.Close()
 
-// 	printTitle(helpText)
+	outputFile, err := os.OpenFile("valid_git_urls.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error opening file valid_git_urls.txt")
+		os.Exit(1)
+	}
+	defer outputFile.Close()
 
-// 	fmt.Println(green("Loaded file: " + file.Name()))
-// 	time.Sleep(time.Second)
-// 	fmt.Println(green("Starting the scanner..."))
-// 	time.Sleep(time.Second)
-// 	s := bufio.NewScanner(file)
-// 	for s.Scan() {
-// 		scanner(s.Text(), outputFile)
-// 	}
-// }
+	printTitle(helpText)
+
+	fmt.Println(green("Loaded file: " + file.Name()))
+	time.Sleep(time.Second)
+	fmt.Println(green("Starting the scanner..."))
+	time.Sleep(time.Second)
+	s := bufio.NewScanner(file)
+	for s.Scan() {
+		sem <- struct{}{}
+		wg.Add(1)
+		go scanner(s.Text(), outputFile)
+	}
+	wg.Wait()
+}
